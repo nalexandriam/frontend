35a425b4aaeac7c595b5ada78a1d463890c3a409
nataam <natadealexandriamenezes@gmail.com>
2021-03-01 04:41:32 -0300

    app agora faz cadastro adicionando logo em base64 de imagem escolhida pelo usuario

--- src/app/cadastro/form-cadastro/form-cadastro.component.ts
+++ src/app/cadastro/form-cadastro/form-cadastro.component.ts
@@ -16,6 +16,8 @@ export class FormCadastroComponent implements OnInit {
   FormCaracteristica: FormGroup;
   caracteristicaOK: boolean = false;
   id_caracteristica: number;
+  base64ok:boolean = false;
+  ImageBase64: any;
 
   constructor(private apiService: ApiService, private fb: FormBuilder) {
     this.createEmpresaForm();
@@ -23,11 +25,11 @@ export class FormCadastroComponent implements OnInit {
    }
    createEmpresaForm() {
     this.FormEmpresa = this.fb.group({
-      logo: 'logo',
-      descricao: 'descricao',
-      mapa: 'mapa',
-      login: 'login',
-      senha: 'senha'
+      logo: '',
+      descricao: '',
+      mapa: '',
+      login: '',
+      senha: ''
     });
   }
 
@@ -48,13 +50,30 @@ export class FormCadastroComponent implements OnInit {
   }
 
   cadastrarEmpresa(FormEmpresa: FormGroup){
-    this.bodyEmpresa['empresa'] = FormEmpresa.getRawValue();
+   
+    let tempEmpresaObj = FormEmpresa.getRawValue();
+    tempEmpresaObj['logo'] = this.ImageBase64.toString();
+    this.bodyEmpresa['empresa'] = tempEmpresaObj; 
     this.bodyEmpresa['empresa']['caracteristica_id'] = this.id_caracteristica;
     this.apiService.cadastroEmpresa(this.bodyEmpresa).subscribe(data =>{
       console.log('data='+data);
     });
   }
 
+  Image(event)
+  {
+    let file:File = event.target.files[0];
+    let base64 = new FileReader();
+    base64.readAsDataURL(file);
+    base64.onload = data=> {
+    //console.log("base64:::::::"+base64.result);
+    this.base64ok = true;
+    this.ImageBase64 = base64.result;
+    };
+    base64.onerror = function (error) {
+      console.log('Error: ', error);
+    };
+  }
   ngOnInit() {}
 
 }
